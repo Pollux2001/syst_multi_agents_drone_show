79b5c81f02a90a1f0059ea91dd525486f4492caf
Eloan <eloantourtelier@gmail.com>
2026-05-22 12:33:11 +0200

    added three dornes scneario and refactored the one drone scneario

--- standalone_sim.py
+++ standalone_sim.py
@@ -16,9 +16,12 @@ nbTb3W = 0
 Tb3W_pose = [[1.0, 0.0, 0.0]]  # x, y, theta
 
 nbRMTT = 3   # dji rmtt drones
-# Started at z=0.0 so we can see the 3s straight line takeoff
+# Started at z=0.0 so we can see the 3s straight line takeoff.
+# The three initial x/y positions match the 120-degree circle formation.
 RMTT_pose = [
     [1.0, 0.0, 0.0],
+    [-0.5, 0.866, 0.0],
+    [-0.5, -0.866, 0.0],
 ]  # x, y, z 
 
 nbCF2 = 0
